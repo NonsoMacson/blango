bc6de9ca4f75b18e1eb7c0d63ce33643eb744231
Chinonso Nwankwo <nonsomacson@gmail.com>
2023-07-14 15:54:09 +0000

    Finish related fields

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,7 +1,8 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, RetrieveUpdateDestroyAPIView
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 from blog.models import Post
-from blog.api.serializers import PostSerializer
+from blog.api.serializers import PostSerializer, Userserializer
+from blango_auth.models import User
 
 
 class PostList(ListCreateAPIView):
@@ -12,3 +13,8 @@ class PostDetail(RetrieveUpdateDestroyAPIView):
   permission_classes=[AuthorModifyOrReadOnly | IsAdminUserForObject]
   queryset=Post.objects.all()
   serializer_class=PostSerializer
+
+class UserDetail(RetrieveAPIView):
+  queryset=User.objects.all()
+  lookup_field="email"
+  serializer_class=Userserializer
