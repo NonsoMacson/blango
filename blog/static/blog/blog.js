d3f35d541c821c7a84b1f91d638959a31dc40cb2
Chinonso Nwankwo <nonsomacson@gmail.com>
2023-07-23 13:52:37 +0000

    Finish JSX

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,36 +1,85 @@
-class ClickButton extends React.Component{
-  state={
-    wasClicked: false
-  }
-
-  handleClicked(){
-    this.setState(
-      {wasClicked: true}
-    )
-  }
-
+class PostRow extends React.Component{
   render(){
-    let buttonText
+    const post=this.props.post
+    
+    let thumbnail
 
-    if(this.state.wasClicked){
-      buttonText="Clicked!"
+    if(post.hero_image.thumbnail){
+      thumbnail=<img src={post.hero_image.thumbnail}/>
     }else{
-      buttonText= "Click me"
+      thumbnail='-'
     }
+    
 
-    return React.createElement(
-      'button',
-      {
-        className: 'btn btn-primary mt-2',
-        onClick: ()=>{
-          this.handleClicked()
-        }
-      }, buttonText
-    )
+    return <tr>
+    <td>{post.title}</td>
+    <td>
+      {thumbnail}
+    </td>
+    <td>{post.tags.join(', ')}</td>
+    <td>{post.slug}</td>
+    <td>{post.summary}</td>
+    <td><a href={'/post/'+ post.slug+ '/'}>View</a></td>
+    </tr>
+    
   }
 }
 
+class PostTable extends React.Component{
+  state = {
+    dataLoaded: true,
+    data: {
+      results: [
+        {
+          id: 15,
+          tags: [
+            'django', 'react'
+          ],
+          'hero_image': {
+            'thumbnail': "http://wonderdenver-signalbook-8000.codio.io/media/__sized__/hero_images/IMG_20191107_190016_001-thumbnail-100x100-70.JPG",
+            'full_size': '/media/hero_images/snake-419043_1920.jpg'
+          },
+          title: 'Test Post',
+          slug: 'test-post',
+          summary: 'A test post, created for Django/React.'
+        }
+      ]
+    }
+  }
+  render(){
+    let rows
+
+    if(this.state.dataLoaded){
+      if (this.state.data.results.length){
+        rows=this.state.data.results.map(post => <PostRow post={post} key={post.id}/>)
+      }else{
+        rows=<tr>
+        <td colspan="6">No results found.</td>
+        </tr>
+      }
+    }else{
+        rows=<tr>
+        <td colspan="6">Loading&hellip;</td>
+        </tr>
+      }
+    return <table className="table table-striped table-bordered mt-2">
+    <thead>
+    <tr>
+      <th>Title</th>
+      <th>Image</th>
+      <th>Tags</th>
+      <th>Slugs</th>
+      <th>Summary</th>
+      <th>Link</th>
+    </tr>
+    </thead>
+    <tbody>
+    {rows}
+    </tbody>
+    </table>
+  }
+}
 
 const domContainer=document.getElementById('react_root')
 
-ReactDOM.render(React.createElement(ClickButton), domContainer)
+ReactDOM.render(React.createElement(PostTable), domContainer)
